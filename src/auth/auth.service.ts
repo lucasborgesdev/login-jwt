bc6fdb8c77727cf527f3d8f056d926b8a1381845
Lucas Borges <lucas_borges_br@hotmail.com>
2021-10-13 23:16:34 -0300

    criando endpoint de confirmação de conta, reset de senha, e update de senha com usuário logado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,6 +12,8 @@ import { UserRole } from '../users/user-roles.enum';
 import { CredentialsDto } from '../users/dtos/credentials.dto';
 import { JwtService } from '@nestjs/jwt';
 import { MailerService } from '@nestjs-modules/mailer';
+import { randomBytes } from 'crypto';
+import { ChangePasswordDto } from './dto/change-password.dto';
 
 @Injectable()
 export class AuthService {
@@ -66,4 +68,60 @@ export class AuthService {
     );
     if (result.affected === 0) throw new NotFoundException('Token inválido');
   }
+
+  async sendRecoverPasswordEmail(email: string): Promise<void> {
+    const user = await this.userRepository.findOne({email})
+
+    if(!user)
+      throw new NotFoundException('Não há usuário cadastrado com esse email.')
+
+    user.recoverToken = randomBytes(32).toString('hex');
+    await user.save();
+
+    const mail = {
+      to: user.email,
+      from: 'noreply@elibras.com',
+      subject: 'Recuperação de Senha',
+      template: 'recover-password',
+      context: {
+        token: user.recoverToken,
+        },
+      };
+    await this.mailerService.sendMail(mail);      
+  }
+
+  async changePassword(
+    id: string,
+    changePasswordDto: ChangePasswordDto,): Promise<void> {
+      const {password, passwordConfirmation} =  changePasswordDto;
+
+      if (password != passwordConfirmation)
+       throw new UnprocessableEntityException('As senhas não conferem');
+
+       await this.userRepository.changePassword(id, password);
+
+    }
+
+    async resetPassword(
+      recoverToken: string,
+      changePasswordDto: ChangePasswordDto,
+    ): Promise<void> {
+      const user = await  this.userRepository.findOne(
+        { recoverToken },
+        {
+          select: ['id'],
+        },
+      );
+      if(!user) throw  new NotFoundException('Token inválido');
+
+      try {
+        await this.changePassword(user.id.toString(), changePasswordDto);
+      } catch (error) {
+        
+        throw error;
+
+      }
+    }
+
+
 }
